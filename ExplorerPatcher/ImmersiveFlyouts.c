495bf904609dd572222354970ef0da6dbd33eab3
RoflCopter4 <brendan.leason.4@gmail.com>
2023-09-15 18:21:34 -0600

    Fix ImmersiveFlyouts.c

--- ExplorerPatcher/ImmersiveFlyouts.c
+++ ExplorerPatcher/ImmersiveFlyouts.c
@@ -1,4 +1,5 @@
 #include "ImmersiveFlyouts.h"
+#include <Shlwapi.h>
 
 void InvokeActionCenter()
 {
--- ExplorerPatcher/ImmersiveFlyouts.h
+++ ExplorerPatcher/ImmersiveFlyouts.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef _H_IMMERSIVEFLYOUTS_H_
 #define _H_IMMERSIVEFLYOUTS_H_
 #include <Windows.h>
